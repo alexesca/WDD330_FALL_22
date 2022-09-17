1d0957e7e58e6d178c8b280433a0c41703a43d29
alexesca <alexescamore@gmail.com>
2022-09-17 11:23:34 -0600

    Added main.js

---
+++ main.js
@@ -0,0 +1,30 @@
+function loadContentTable() {
+
+            // Sets content table content as array
+            var links = [
+                {
+                    label: "W01",
+                    url: "W01/Local Storage/index.html"
+                }
+            ];
+
+            var ol = document.getElementById('content-table');
+
+            links.map(item => {    
+                var li = document.createElement('li');
+                var a = createLink(item.label, item.url);
+                entry.appendChild(a);
+                list.appendChild(entry);
+            })
+        
+        }
+
+        function createLink(title, link) {
+            // Creates link
+            var a = document.createElement('a');
+            var linkText = document.createTextNode(title);
+            a.appendChild(linkText);
+            a.title = title;
+            a.href = link;
+            document.body.appendChild(a);
+        }
